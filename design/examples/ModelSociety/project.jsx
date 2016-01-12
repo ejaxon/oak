1457232a87dfa0d06031600d4980eeb0519a5a08
owenoak <owenoak@gmail.com>
2016-01-12 06:55:40 -0500

    sample ModelSociety project stub

---
+++ design/examples/ModelSociety/project.jsx
@@ -0,0 +1,15 @@
+<Project id="ModelSociety">
+	<platform type="desktop, tablet">
+		<Layout id="UI">
+			<Component type="AppToolbar" path="/components/siteToolbar"/>
+		</Layout>
+	</platform>
+
+	<Platform type="mobile">
+		<Layout id="UI">
+			<Sidebar location="left">
+				<Component type="AppToolbar" path="/components/siteToolbar"/>
+			</Sidebar>
+		</Layout>
+	</Platform>
+</Project>
