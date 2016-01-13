547d3bc67386d78f0f4c62fae19c1dc261b904f9
owenoak <owenoak@gmail.com>
2016-01-12 21:07:52 -0500

    ModelSociety example AirBnB-ish es2015 eslinting in Atom

---
+++ examples/ModelSociety/project/project.jsx
@@ -0,0 +1,5 @@
+<Project id="ModelSociety" {...props}>
+  <Component ref="header" path="ProjectHeader"/>
+  <CurrentStack ref="stack" {...props.stackProps}/>
+  <Component ref="footer" path="ProjectFooter"/>
+</Project>
